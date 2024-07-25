205223e16a0d6ca89e586a2349552c8c05f7ca0f
Saqib Ahmed <75667871+mughaltechlab@users.noreply.github.com>
2024-07-25 14:46:03 +0500

    update

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 // pkgs import
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
-import { app, server } from "./socket/socket.js";
 
 // files import
 import authRoutes from "./routes/auth.routes.js";
@@ -11,11 +11,13 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 // variables
 // const app = express();
 const PORT = process.env.PORT || 8000;
+const __dirname = path.resolve();
 
 //:: APP MIDDLEWARE
 
@@ -26,6 +28,12 @@ app.use(cookieParser());    // to parse the incoming cookies form req.cookies
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname,"frontend", "dist", "index.html"));
+})
 
 // app.get('/',(req,res)=>{
 //     // root route http://localhost:5000/
