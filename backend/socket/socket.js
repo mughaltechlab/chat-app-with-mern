5a923ccf153ee37e157ae10b6ce99c579d34eab3
Saqib Ahmed <75667871+mughaltechlab@users.noreply.github.com>
2024-08-01 19:49:47 +0500

    update origin

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ["https://chat-app-with-mern-weld.vercel.app"],
+        origin: ["http://localhost:3000"],
         methods: ["GET","POST"]
     }
 });
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port:3000,
     proxy: {
       "/api": {
-        target: "https://chat-app-with-mern-weld.vercel.app",
+        target: "http://localhost:8000",
       }
     }
   },
